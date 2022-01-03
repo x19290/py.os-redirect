c5e7d8fc9312a082052dab25b4265fd365bf90cc
Hiroki Horiuchi <x19290@gmail.com>
2022-01-03 20:38:58 +0900

    refactor(ConcurrentReader): s/b.decode()/utf8decode(b)/

--- osredirect/thread/__init__.py
+++ osredirect/thread/__init__.py
@@ -26,7 +26,8 @@ class ConcurrentReader(ThreadTuple):
                 return b
         else:
             def adapt(b):
-                return b.decode(r'UTF-8')  # TODO: use faster way
+                from ..codecs.utf8 import utf8decode
+                return utf8decode(b)
 
         def pump(fd, oobj):
             while True:
