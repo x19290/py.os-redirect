bec35d762ff00144e776f3d1f933383367f8cd46
Hiroki Horiuchi <x19290@gmail.com>
2022-01-03 20:46:38 +0900

    refactor(zz0redirect_t.py): abstract _Test out from T0

--- osredirect/zz0redirect_t.py
+++ osredirect/zz0redirect_t.py
@@ -7,47 +7,54 @@ from unittest import TestCase
 _BOTH = STDOUT_BIT | STDERR_BIT
 
 
-class T0(TestCase):
+class _Test:
     def _test(self, exec, fdbits, expected, *mems):
+        expected, sep = map(self.adapt, (expected, r' '))
         redirect(exec, fdbits, *mems)
-        actual = br' '.join(y.getvalue() for y in mems)
+        actual = sep.join(y.getvalue() for y in mems)
         eq_(expected, actual)
 
     def test00(self):
-        self._test(w1, STDOUT_BIT, br'')
+        self._test(w1, STDOUT_BIT, r'')
 
     def test01(self):
-        self._test(w1, STDOUT_BIT, br'1', BytesIO())
+        self._test(w1, STDOUT_BIT, r'1', BytesIO())
 
     def test02(self):
-        self._test(w1, STDOUT_BIT, br'1 ', BytesIO(), BytesIO())
+        self._test(w1, STDOUT_BIT, r'1 ', BytesIO(), BytesIO())
 
     def test03(self):
-        self._test(w1, STDOUT_BIT, br'1  ', BytesIO(), BytesIO(), BytesIO())
+        self._test(w1, STDOUT_BIT, r'1  ', BytesIO(), BytesIO(), BytesIO())
 
     def test10(self):
-        self._test(w2, STDERR_BIT, br'')
+        self._test(w2, STDERR_BIT, r'')
 
     def test11(self):
-        self._test(w2, STDERR_BIT, br'2', BytesIO())
+        self._test(w2, STDERR_BIT, r'2', BytesIO())
 
     def test12(self):
-        self._test(w2, STDERR_BIT, br'2 ', BytesIO(), BytesIO())
+        self._test(w2, STDERR_BIT, r'2 ', BytesIO(), BytesIO())
 
     def test13(self):
-        self._test(w2, STDERR_BIT, br'2  ', BytesIO(), BytesIO(), BytesIO())
+        self._test(w2, STDERR_BIT, r'2  ', BytesIO(), BytesIO(), BytesIO())
 
     def test20(self):
-        self._test(w1w2, _BOTH, br'')
+        self._test(w1w2, _BOTH, r'')
 
     def test21(self):
-        self._test(w1w2, _BOTH, br'1', BytesIO())
+        self._test(w1w2, _BOTH, r'1', BytesIO())
 
     def test22(self):
-        self._test(w1w2, _BOTH, br'1 2', BytesIO(), BytesIO())
+        self._test(w1w2, _BOTH, r'1 2', BytesIO(), BytesIO())
 
     def test23(self):
-        self._test(w1w2, _BOTH, br'1 2 ', BytesIO(), BytesIO(), BytesIO())
+        self._test(w1w2, _BOTH, r'1 2 ', BytesIO(), BytesIO(), BytesIO())
+
+
+class T0(_Test, TestCase):
+    @staticmethod
+    def adapt(s):
+        return s.encode(r'UTF-8')
 
 
 def w1():
