afd68b0d8f4a2c98dd51895c0d0cd4fa1a049b20
Hiroki Horiuchi <x19290@gmail.com>
2022-01-03 21:02:41 +0900

    fix(zz0redirect_t.py): fix the test bug

--- osredirect/zz0redirect_t.py
+++ osredirect/zz0redirect_t.py
@@ -1,5 +1,5 @@
 from . import redirect, STDERR_BIT, STDERR, STDOUT, STDOUT_BIT
-from io import BytesIO
+from io import BytesIO, StringIO
 from os import write
 from nose.tools import eq_
 from unittest import TestCase
@@ -18,46 +18,56 @@ class _Test:
         self._test(w1, STDOUT_BIT, r'')
 
     def test01(self):
-        self._test(w1, STDOUT_BIT, r'1', BytesIO())
+        self._test(w1, STDOUT_BIT, r'1', self.memio())
 
     def test02(self):
-        self._test(w1, STDOUT_BIT, r'1 ', BytesIO(), BytesIO())
+        memio = self.memio
+        self._test(w1, STDOUT_BIT, r'1 ', memio(), memio())
 
     def test03(self):
-        self._test(w1, STDOUT_BIT, r'1  ', BytesIO(), BytesIO(), BytesIO())
+        memio = self.memio
+        self._test(w1, STDOUT_BIT, r'1  ', memio(), memio(), memio())
 
     def test10(self):
         self._test(w2, STDERR_BIT, r'')
 
     def test11(self):
-        self._test(w2, STDERR_BIT, r'2', BytesIO())
+        self._test(w2, STDERR_BIT, r'2', self.memio())
 
     def test12(self):
-        self._test(w2, STDERR_BIT, r'2 ', BytesIO(), BytesIO())
+        memio = self.memio
+        self._test(w2, STDERR_BIT, r'2 ', memio(), memio())
 
     def test13(self):
-        self._test(w2, STDERR_BIT, r'2  ', BytesIO(), BytesIO(), BytesIO())
+        memio = self.memio
+        self._test(w2, STDERR_BIT, r'2  ', memio(), memio(), memio())
 
     def test20(self):
         self._test(w1w2, _BOTH, r'')
 
     def test21(self):
-        self._test(w1w2, _BOTH, r'1', BytesIO())
+        self._test(w1w2, _BOTH, r'1', self.memio())
 
     def test22(self):
-        self._test(w1w2, _BOTH, r'1 2', BytesIO(), BytesIO())
+        memio = self.memio
+        self._test(w1w2, _BOTH, r'1 2', memio(), memio())
 
     def test23(self):
-        self._test(w1w2, _BOTH, r'1 2 ', BytesIO(), BytesIO(), BytesIO())
+        memio = self.memio
+        self._test(w1w2, _BOTH, r'1 2 ', memio(), memio(), memio())
 
 
 class T0bin(_Test, TestCase):
+    memio = BytesIO
+
     @staticmethod
     def adapt(s):
         return s.encode(r'UTF-8')
 
 
 class T1str(_Test, TestCase):
+    memio = StringIO
+
     @staticmethod
     def adapt(s):
         return s
