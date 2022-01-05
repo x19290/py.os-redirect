b4ae116b6982bf7a7a59fd40ba06883f60f00007
Hiroki Horiuchi <x19290@gmail.com>
2022-01-05 09:36:58 +0900

    chore(thread/zztest.py): avoid "not used" warnings

--- osredirect/thread/zztest.py
+++ osredirect/thread/zztest.py
@@ -4,3 +4,11 @@ from os import close, pipe, read, write
 from nose.tools import eq_
 from threading import Thread
 from unittest import TestCase
+(
+    ConcurrentReader, ThreadTuple,
+    BytesIO, StringIO,
+    close, pipe, read, write,
+    eq_,
+    Thread,
+    TestCase,
+)  # to avoid "not used" warnings
