a171cd5a3b8675a32400159cf35ba1cdad4e139f
Hiroki Horiuchi <x19290@gmail.com>
2022-01-07 17:17:08 +0900

    refactor(redirect_test.py): simplify it

--- x19290/redirect/redirect_test.py
+++ x19290/redirect/redirect_test.py
@@ -6,35 +6,32 @@ from unittest import TestCase
 
 
 class _WithoutStdin:
-    def _test(self, n):
-        expected, isep, osep = map(self.adapt, (self.expected, r',', r' '))
-        expected = expected.split(isep)[n]
-        mems = tuple(self.memio() for _ in range(n))
-        outs = self.outs
-        kwargs = dict(stdin=None, stdout=None, stderr=None)
-        for k, m in zip(outs, mems):
-            kwargs[k] = m
+    def _test(self, expected, *outs):
+        adapt = self.adapt
+        expected, sep = map(adapt, (expected, r' '))
+        memio, kwargs = self.memio, {}
+        for k in outs:
+            kwargs[k] = memio()
         with redirect(**kwargs) as ischild:
             if ischild:
-                if kwargs[r'stdout']:
+                if r'stdout' in kwargs:
                     write(STDOUT, br'1')
-                if kwargs[r'stderr']:
+                if r'stderr' in kwargs:
                     write(STDERR, br'2')
-        actual = (kwargs[y] for y in outs)
-        actual = osep.join(y.getvalue() for y in actual if y)
+        actual = sep.join(kwargs[y].getvalue() for y in outs)
         eq_(expected, actual)
 
     def test0(self):
-        self._test(0)
+        self._test(r'')
 
     def test1(self):
-        self._test(1)
+        self._test(r'1', r'stdout')
 
     def test2(self):
-        self._test(2)
+        self._test(r'1 2', r'stdout', r'stderr')
 
 
-class _0bin(_WithoutStdin):
+class T0bin(_WithoutStdin, TestCase):
     memio = BytesIO
 
     @staticmethod
@@ -42,51 +39,12 @@ class _0bin(_WithoutStdin):
         return s.encode(r'UTF-8')
 
 
-class _1str(_WithoutStdin):
+class T1str(_WithoutStdin, TestCase):
     memio = StringIO
 
     @staticmethod
     def adapt(s):
         return s
-
-
-class _0w1:
-    expected = r',1,1'
-    outs = r'stdout',
-
-
-class _1w2:
-    expected = r',2,2'
-    outs = r'stderr',
-
-
-class _2w1w2:
-    expected = r',1,1 2'
-    outs = r'stdout', r'stderr'
-
-
-class T0bin0w1(_0bin, _0w1, TestCase):
-    pass
-
-
-class T0bin1w2(_0bin, _1w2, TestCase):
-    pass
-
-
-class T0bin2w1w2(_0bin, _2w1w2, TestCase):
-    pass
-
-
-class T1str0w1(_1str, _0w1, TestCase):
-    pass
-
-
-class T1str1w2(_1str, _1w2, TestCase):
-    pass
-
-
-class T1str2w1w2(_1str, _2w1w2, TestCase):
-    pass
 
 
 class T2with_stdin(TestCase):
